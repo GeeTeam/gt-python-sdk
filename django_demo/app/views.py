8234553bdb5aa644c82117a96aab9d33e96d2a97
王沛丰 <imelucifer@foxmail.com>
2015-04-01 11:49:44 +0800

    fix

--- django_demo/app/views.py
+++ django_demo/app/views.py
@@ -2,18 +2,24 @@ from django.shortcuts import render_to_response, RequestContext
 from django.http import HttpResponse
 import sdk.geetest as geetest
 
+BASE_URL = "api.geetest.com/get.php?gt="
 
-captcha_id = "17e0a0be98b00ed5e5b263931f3156d9"
-private_key = "*****"
+captcha_id = "a40fd3b0d712165c5d13e6f747e948d4"
+private_key = "0f1a37e33c9ed10dd2e133fe2ae9c459"
 
 
 def home(request):
     gt = geetest.geetest(captcha_id, private_key)
+    url = ""
+    httpsurl = ""
     try:
         challenge = gt.geetest_register()
     except:
         challenge = ""
-    return render_to_response("index.html", {"challenge": challenge}, context_instance=RequestContext(request))
+    if len(challenge) == 32:
+        url = "http://%s%s&challenge=%s" % (BASE_URL, captcha_id, challenge)
+        httpsurl = "https://%s%s&challenge=%s" % (BASE_URL, captcha_id, challenge)
+    return render_to_response("index.html", {"url": url}, context_instance=RequestContext(request))
 
 
 def login(request):
