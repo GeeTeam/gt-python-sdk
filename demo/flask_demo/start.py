f35cb43a097a53cacf7f1af0fb0dcb2b0f26f642
parcuse <xin532222767@qq.com>
2016-09-18 10:50:31 +0800

    fix tornado and flask demo

--- demo/flask_demo/start.py
+++ demo/flask_demo/start.py
@@ -1,32 +1,42 @@
-# coding:utf-8
+#coding:utf-8
 import json
 
 from flask import session, make_response, Flask, request, render_template
 from geetest import GeetestLib
 
-captcha_id = "b46d1900d0a894591916ea94ea91bd2c"
-private_key = "36fc3fe98530eea08dfc6ce76e3d24c4"
-
+pc_geetest_id = "b46d1900d0a894591916ea94ea91bd2c"
+pc_geetest_key = "36fc3fe98530eea08dfc6ce76e3d24c4"
+mobile_geetest_id = "7c25da6fe21944cfe507d2f9876775a9"
+mobile_geetest_key = "f5883f4ee3bd4fa8caec67941de1b903"
 app = Flask(__name__)
 app.config.update(
     DEBUG=True,
 )
 
 
-@app.route('/register', methods=["GET"])
-def get_captcha():
+@app.route('/pc-geetest/register', methods=["GET"])
+def get_pc_captcha():
     user_id = 'test'
-    gt = GeetestLib(captcha_id, private_key)
+    gt = GeetestLib(pc_geetest_id, pc_geetest_key)
     status = gt.pre_process(user_id)
     session[gt.GT_STATUS_SESSION_KEY] = status
     session["user_id"] = user_id
     response_str = gt.get_response_str()
     return response_str
 
+@app.route('/mobile-geetest/register', methods=["GET"])
+def get_mobile_captcha():
+    user_id = 'test'
+    gt = GeetestLib(mobile_geetest_id, mobile_geetest_key)
+    status = gt.pre_process(user_id)
+    session[gt.GT_STATUS_SESSION_KEY] = status
+    session["user_id"] = user_id
+    response_str = gt.get_response_str()
+    return response_str
 
-@app.route('/validate', methods=["POST"])
-def validate_capthca():
-    gt = GeetestLib(captcha_id, private_key)
+@app.route('/pc-geetest/validate', methods=["POST"])
+def pc_validate_captcha():
+    gt = GeetestLib(pc_geetest_id, pc_geetest_key)
     challenge = request.form[gt.FN_CHALLENGE]
     validate = request.form[gt.FN_VALIDATE]
     seccode = request.form[gt.FN_SECCODE]
@@ -34,16 +44,29 @@ def validate_capthca():
     user_id = session["user_id"]
     if status:
         result = gt.success_validate(challenge, validate, seccode, user_id)
-        print 111
     else:
         result = gt.failback_validate(challenge, validate, seccode)
-        print 222
     result = "<html><body><h1>登录成功</h1></body></html>" if result else "<html><body><h1>登录失败</h1></body></html>"
     return result
 
-@app.route('/ajax_validate', methods=["POST"])
-def ajax_validate():
-    gt = GeetestLib(captcha_id, private_key)
+@app.route('/pc-geetest/ajax_validate', methods=["POST"])
+def pc_ajax_validate():
+    gt = GeetestLib(pc_geetest_id,pc_geetest_key)
+    challenge = request.form[gt.FN_CHALLENGE]
+    validate = request.form[gt.FN_VALIDATE]
+    seccode = request.form[gt.FN_SECCODE]
+    status = session[gt.GT_STATUS_SESSION_KEY]
+    user_id = session["user_id"]
+    if status:
+        result = gt.success_validate(challenge, validate, seccode, user_id,data='',userinfo='')
+    else:
+        result = gt.failback_validate(challenge, validate, seccode)
+    result = {"status":"success"} if result else {"status":"fail"}
+    return json.dumps(result)
+
+@app.route('/mobile-geetest/ajax_validate', methods=["POST"])
+def mobile_ajax_validate():
+    gt = GeetestLib(mobile_geetest_id,mobile_geetest_key)
     challenge = request.form[gt.FN_CHALLENGE]
     validate = request.form[gt.FN_VALIDATE]
     seccode = request.form[gt.FN_SECCODE]
--- demo/tornado_demo/start.py
+++ demo/tornado_demo/start.py
@@ -7,8 +7,10 @@ from geetest import GeetestLib
 
 import json
 
-captcha_id = "b46d1900d0a894591916ea94ea91bd2c"
-private_key = "36fc3fe98530eea08dfc6ce76e3d24c4"
+pc_geetest_id = "b46d1900d0a894591916ea94ea91bd2c"
+pc_geetest_key = "36fc3fe98530eea08dfc6ce76e3d24c4"
+mobile_geetest_id = "7c25da6fe21944cfe507d2f9876775a9"
+mobile_geetest_key = "f5883f4ee3bd4fa8caec67941de1b903"
 
 product = "embed"
 # 弹出式
@@ -20,20 +22,29 @@ class MainHandler(tornado.web.RequestHandler):
         self.render("static/login.html",)
 
 
-class GetCaptchaHandler(SessionBaseHandler):
+class PcGetCaptchaHandler(SessionBaseHandler):
     def get(self):
         user_id = 'test'
-        gt = GeetestLib(captcha_id, private_key)
+        gt = GeetestLib(pc_geetest_id, pc_geetest_key)
         status = gt.pre_process(user_id)
         self.session[gt.GT_STATUS_SESSION_KEY] = status
         self.session["user_id"] = user_id
         response_str = gt.get_response_str()
         self.write(response_str)
 
+class MobileGetCaptchaHandler(SessionBaseHandler):
+    def get(self):
+        user_id = 'test'
+        gt = GeetestLib(mobile_geetest_id, mobile_geetest_key)
+        status = gt.pre_process(user_id)
+        self.session[gt.GT_STATUS_SESSION_KEY] = status
+        self.session["user_id"] = user_id
+        response_str = gt.get_response_str()
+        self.write(response_str)
 
-class ValidateHandler(SessionBaseHandler):
+class PcValidateHandler(SessionBaseHandler):
     def post(self):
-        gt = GeetestLib(captcha_id, private_key)
+        gt = GeetestLib(pc_geetest_id, pc_geetest_key)
         challenge = self.get_argument(gt.FN_CHALLENGE, "")
         validate = self.get_argument(gt.FN_VALIDATE, "")
         seccode = self.get_argument(gt.FN_SECCODE, "")
@@ -48,9 +59,25 @@ class ValidateHandler(SessionBaseHandler):
         self.write(result)
 
 
-class AjaxValidateHandler(SessionBaseHandler):
+class PcAjaxValidateHandler(SessionBaseHandler):
     def post(self):
-        gt = GeetestLib(captcha_id, private_key)
+        gt = GeetestLib(pc_geetest_id, pc_geetest_key)
+        challenge = self.get_argument(gt.FN_CHALLENGE, "")
+        validate = self.get_argument(gt.FN_VALIDATE, "")
+        seccode = self.get_argument(gt.FN_SECCODE, "")
+        status = self.session[gt.GT_STATUS_SESSION_KEY]
+        user_id = self.session["user_id"]
+        if status:
+            result = gt.success_validate(challenge, validate, seccode, user_id)
+        else:
+            result = gt.failback_validate(challenge, validate, seccode)
+            self.session["user_id"] = user_id
+        result = result = {"status":"success"} if result else {"status":"fail"}
+        self.write(json.dumps(result))
+
+class MobileAjaxValidateHandler(SessionBaseHandler):
+    def post(self):
+        gt = GeetestLib(mobile_geetest_id, mobile_geetest_key)
         challenge = self.get_argument(gt.FN_CHALLENGE, "")
         validate = self.get_argument(gt.FN_VALIDATE, "")
         seccode = self.get_argument(gt.FN_SECCODE, "")
@@ -67,9 +94,11 @@ class AjaxValidateHandler(SessionBaseHandler):
 if __name__ == "__main__":
     app = tornado.web.Application([
                                       (r"/", MainHandler),
-                                      (r"/register", GetCaptchaHandler),
-                                      (r"/validate", ValidateHandler),
-                                      (r"/ajax_validate", AjaxValidateHandler)
+                                      (r"/pc-geetest/register", PcGetCaptchaHandler),
+                                      (r"/mobile-geetest/register",MobileGetCaptchaHandler),
+                                      (r"/pc-geetest/validate", PcValidateHandler),
+                                      (r"/pc-geetest/ajax_validate", PcAjaxValidateHandler),
+                                      (r"/mobile-geetest/ajax_validate",MobileAjaxValidateHandler)
                                   ], debug=True)
 
     app.listen(8088)
